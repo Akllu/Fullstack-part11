5e62dce24544b470c965b976dab66bf532f40eff
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2022-08-09 20:18:11 +0300

    Modified tests

--- cypress/e2e/bloglist.cy.js
+++ cypress/e2e/bloglist.cy.js
@@ -1,13 +1,13 @@
 describe('Bloglist', function () {
   it('login form is shown', function () {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:5000')
     cy.contains('Login')
     cy.contains('Username')
     cy.contains('Password')
   })
 
   it('login succeeds with correct credentials', function () {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:5000')
     cy.get('#username-input').type('wick')
     cy.get('#password-input').type('john123')
     cy.get('#login-button').click()
